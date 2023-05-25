fd1ac26ed0ede4a337cce98df52c02da6ba9fc84
Ningxiao Yang <z5361584@ad.unsw.edu.au>
2023-05-25 23:44:21 +1000

    更新了一些和owner有关的功能

--- Skillverse/backend/contracts/SkillContract.sol
+++ Skillverse/backend/contracts/SkillContract.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.9;
 
 // 处理AI返回的结果，并将结果返回给前端
 contract SkillContract {
+
+    // 监听事件给前端
+    event NewMonster(uint id, string name);
+
     // 设置Monster的技能initial
     struct Skill{
         uint skillID;
@@ -22,30 +26,44 @@ contract SkillContract {
     // key为monsterID，value为Monster
     // 创造一个map记录所有monsters
     // 其他合约可以通过指定怪物的 ID 来获取对应的 Monster
-    mapping(uint => Monster) public monsters;
+    mapping(uint => Monster) private monsters;
+
+    // 记录怪物ID拥有者的地址
+    mapping(uint => address) public monsterToOwner;
+
+    // 记录某地址拥有怪物的数量
+    mapping(address => uint) ownerMonsterCount;
 
     // 测试，查看skills里的所有技能
-    function getMonsterSkills(uint _monsterID) public view returns (Skill[] memory) {
+    function getMonsterSkills(uint _monsterID) private view returns (Skill[] memory) {
         return monsters[_monsterID].skills;
     }
 
-
     // 添加新Monster
-    function addMonster(uint _monsterID, string memory _monsterName) public {
+    function addMonster(uint _monsterID, string storage _monsterName) private {
         Monster storage newMonster = monsters[_monsterID];
+        // 把生成的monster加进用户地址
+        monsterToOwner[_monsterID] = msg.sender;
+        ownerMonsterCount[msg.sender]++;
+        // 用户最多生成5个monsters
+        require(ownerMonsterCount[msg.sender] <= 5);
+        
+        // 检查用户有无重复生成(待更新)
+        
         newMonster.monsterID = _monsterID;
         newMonster.monsterName = _monsterName;
+        
+        emit NewMonster(_monsterID, _monsterName);
     }
 
     // 添加新Skill
-    // 需要根据skillLevel进行sort
-    function addSkill(uint _monsterID, uint _skillLevel, uint _skillID, string memory _skillDescription, uint _skillPoint) public {
+    function addSkill(uint _monsterID, uint _skillLevel, uint _skillID, string storage _skillDescription, uint _skillPoint) private {
         Monster storage monster = monsters[_monsterID];
         monster.skills.push(Skill(_skillID, _skillLevel, _skillDescription, _skillPoint, false));
     }
 
     // 学会skill
-    function learnSkill(uint _monsterID, uint _skillID) public {
+    function learnSkill(uint _monsterID, uint _skillID) private {
         // 找到该技能所属的Monster先
         // skillID不在Monster的skills范围内，失效
         Monster storage monster = monsters[_monsterID];
