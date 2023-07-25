9f8ca6bade6a64e73a6591c6875c703fac3e0bd1
Ningxiao Yang <z5361584@ad.unsw.edu.au>
2023-07-25 20:36:45 +1000

    fixed skill printing twice error

--- Skillverse/contacts/src/pages/Input.jsx
+++ Skillverse/contacts/src/pages/Input.jsx
@@ -1,110 +1,67 @@
-import React from 'react';
-import { useState, useEffect } from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import BigButton from '../components/BigButton';
+import PropTypes from 'prop-types';
 
 const Input = (props) => {
-    const [position, setPosition] = useState("")
-    const [areaOfInterest, setAreaOfInterest] = useState('');
-    const filtered_Input = `what skills do I need to learn if I want to become a ${position}? Answer in tree data structure format without any extra words, if learning skill1 depending on skill2 then skill 2 should be parent node of skill 1.\n\
-  Answer in the following format:\n\
-Skill, SkillID, Parent, ParentID| Here is an example to follow:\n\
-HTML, 1, None, 0|CSS, 2, None, 0|JavaScript, 3, None, 0|DOM Manipulation, 4, JavaScript, 3|CSS Frameworks, 5, CSS, 2|Bootstrap, 6, CSS Frameworks, 5|JavaScript Libraries, 7, JavaScript, 3|jQuery, 8, JavaScript Libraries, 7|React, 9, JavaScript Libraries, 7|Redux, 10, React, 9|Angular, 11, JavaScript Libraries, 7`
-    const [message, setMessage] = useState(null)
-    const [content, setContent] = useState([])
-    const [skillArrays, setSkillArrays] = useState([])
+  const [position, setPosition] = useState('');
+  const [areaOfInterest, setAreaOfInterest] = useState('');
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleInterestChange = (event) => {
+    setAreaOfInterest(event.target.value);
+  };
 
-    // handle change when user selects an option for area of interest
-    const handleInterestChange = (event) => {
-        setAreaOfInterest(event.target.value);
-    };
+  const handlePositionChange = (event) => {
+    setPosition(event.target.value);
+    props.setUserInputFn(event.target.value);
+    localStorage.setItem('userInput', event.target.value);
+  };
 
-    // handle change when user selects an option for area of interest
-    const handlePositionChange = (event) => {
-        setPosition(event.target.value);
-    };
-    
-    useEffect(() => {
-        if (position && message && skillArrays) {
-            setContent(skillArrays)
-        }
-    }, [message, skillArrays])  
+  const getPositions = () => {
+    // Handle getting positions based on area of interest
+    // TO-DO
+  };
 
-    const getPositions = () => {
-        // TO-DO
-    }
+  const getSkillInfos = () => {
+    navigate(`/monster-output`);
+  };
 
-    const getSkillInfos = async () => {
-        const options = {
-            method: 'POST',
-            headers: {
-                "Content-type": 'application/json',
-            },
-            body: JSON.stringify ({
-                message: filtered_Input,
-            })
-        }
-        try {
-            const response = await fetch('http://localhost:8000/completions', options)
-            const data = await response.json()
-            setMessage(data.choices[0].message)
-            const MsgContent = data.choices[0].message.content
-
-            // Split message into skillInfos
-            const skillInfos = MsgContent.split("|")
-            for (const InfoString of skillInfos) {
-                const InfoArray = InfoString.split(", ")
-                setSkillArrays( prevs => (
-                    [...prevs,   
-                        {
-                            Skill: InfoArray[0],
-                            SkillID: InfoArray[1],
-                            Parent: InfoArray[2],
-                            ParentID: InfoArray[3]             
-                        }
-                    ]
-                ))
-                console.log(InfoArray)
-            }
-
-        } catch (error) {
-            console.log(error)
-        }
-        navigate(`/monster-output?position=${position}&message=${encodeURIComponent(message)}&skillArrays=${encodeURIComponent(JSON.stringify(skillArrays))}`);
-    }
-
-    return (
-        <div className="app">
-            <section className='input-container'>
-                <div className='interest-input'>
-                    <h1>What is your area of interest?</h1>
-                    <select value={areaOfInterest} onChange={handleInterestChange}>
-                        <option value="">Select an option</option>
-                        <option value="Information Technology">Information Technology</option>
-                        <option value="Creative Arts">Creative Arts</option>
-                        <option value="Engineering">Engineering</option>
-                    </select>
-                    <BigButton id='confirm' onClick={getPositions}> Confirm </BigButton>
-                </div>
-                <br/>
-                <div className='position-input'>
-                    <h1>What is your career goal?</h1>
-                    {/* Just to show */}
-                    <input value={position} onChange={(e) => setPosition(e.target.value)}/>
-                    <BigButton id='confirm' onClick={getSkillInfos}> Confirm </BigButton>
-                    
-                    <select value={position} onChange={handlePositionChange}>
-                        <option value="">Select an option</option>
-                        <option value="Frontend developer">Frontend developer</option>
-                        <option value="Backend developer">Backend developer</option>
-                        <option value="Game developer">Game developer</option>
-                    </select>
-                </div>
-            </section>
+  return (
+    <div className="app">
+      <section className="input-container">
+        <div className="interest-input">
+          <h1>What is your area of interest?</h1>
+          <select value={areaOfInterest} onChange={handleInterestChange}>
+            <option value="">Select an option</option>
+            <option value="Information Technology">Information Technology</option>
+            <option value="Creative Arts">Creative Arts</option>
+            <option value="Engineering">Engineering</option>
+          </select>
+          <BigButton id="confirm" onClick={getPositions}>
+            Confirm
+          </BigButton>
         </div>
-    );
+        <br />
+        <div className="position-input">
+          <h1>What is your career goal?</h1>    
+          <select value={position} onChange={handlePositionChange}>
+            <option value="">Select an option</option>
+            <option value="Frontend developer">Frontend developer</option>
+            <option value="Backend developer">Backend developer</option>
+            <option value="Game developer">Game developer</option>
+          </select>
+          <BigButton id="confirm" onClick={getSkillInfos}>
+            Confirm
+          </BigButton>
+        </div>
+      </section>
+    </div>
+  );
 };
+
+Input.protoTypes = {
+    setUserInputFn: PropTypes.func
+}
 
 export default Input;
--- Skillverse/contacts/src/pages/Output.jsx
+++ Skillverse/contacts/src/pages/Output.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import './Output.css'
+import PropTypes from 'prop-types';
+import '../pages/Output.css';
 
-const Output = () => {
-  const [position, setPosition] = useState("");
-  const [message, setMessage] = useState(null);
-  const [content, setContent] = useState([]);
-  const location = useLocation();
+const Output = (props) => {
+  const [prevSkill, setprevSkill] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [skillArrays, setSkillArrays] = useState([]);
+  const [gotInfo, setGotInfo] = useState(false);
+
+  // debug
+  const [debug, setDebug] = useState("")
 
   const getSkillInfos = async () => {
-    const filtered_Input = `what skills do I need to learn if I want to become a ${position}? Answer in tree data structure format without any extra words, if learning skill1 depending on skill2 then skill 2 should be parent node of skill 1.\n\
-    Answer in the following format:\n\
-    Skill, SkillID, Parent, ParentID| Here is an example to follow:\n\
-    HTML, 1, None, 0|CSS, 2, None, 0|JavaScript, 3, None, 0|DOM Manipulation, 4, JavaScript, 3|CSS Frameworks, 5, CSS, 2|Bootstrap, 6, CSS Frameworks, 5|JavaScript Libraries, 7, JavaScript, 3|jQuery, 8, JavaScript Libraries, 7|React, 9, JavaScript Libraries, 7|Redux, 10, React, 9|Angular, 11, JavaScript Libraries, 7`;
+    setGotInfo(true)
+    const filtered_Input = `what skills do I need to learn if I want to become a ${props.userInput}? Answer in tree data structure format without any extra words, if learning skill1 depending on skill2 then skill 2 should be parent node of skill 1.\n
+  Answer in the following format:\n
+  Skill, SkillID, Parent, ParentID| Here is an example to follow:\n
+  HTML, 1, None, 0|CSS, 2, None, 0|JavaScript, 3, None, 0|DOM Manipulation, 4, JavaScript, 3|CSS Frameworks, 5, CSS, 2|Bootstrap, 6, CSS Frameworks, 5|JavaScript Libraries, 7, JavaScript, 3|jQuery, 8, JavaScript Libraries, 7|React, 9, JavaScript Libraries, 7|Redux, 10, React, 9|Angular, 11, JavaScript Libraries, 7`;
 
     const options = {
       method: 'POST',
@@ -27,61 +31,70 @@ const Output = () => {
     try {
       const response = await fetch('http://localhost:8000/completions', options);
       const data = await response.json();
-
-      if (data.choices && data.choices.length > 0) {
-        const firstChoice = data.choices[0];
-
-        if (firstChoice.message) {
-          setMessage(firstChoice.message);
-          const MsgContent = firstChoice.message.content;
-
-          // Split message into skillInfos
-          const skillInfos = MsgContent.split("|");
-          const skills = skillInfos.map((InfoString) => {
-            const InfoArray = InfoString.split(", ");
-            return {
-              Skill: InfoArray[0],
-              SkillID: InfoArray[1],
-              Parent: InfoArray[2],
-              ParentID: InfoArray[3]
-            };
-          });
-          setContent(skills);
-        }
+      setDebug(data.choices[0].message.content)
+      const MsgContent = data.choices[0].message.content;
+      // Split message into skillInfos
+      const skillInfos = MsgContent.split("|");
+      console.log(skillInfos.length)
+      for (const InfoString of skillInfos) {
+        const InfoArray = InfoString.split(", ");
+        setSkillArrays( prevs => (
+          [...prevs,   
+              {
+                  Skill: InfoArray[0],
+                  SkillID: InfoArray[1],
+                  Parent: InfoArray[2],
+                  ParentID: InfoArray[3]             
+              }
+          ]
+        ))
+        console.log("hello")
       }
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const outputPosition = searchParams.get('position');
-    const outputMessage = searchParams.get('message');
+    if (props.userInput && skillArrays){  
+      getSkillInfos();
+      setprevSkill(skillArrays); 
+    }
+  }, [skillArrays]); 
 
-    setPosition(outputPosition);
-    setMessage(outputMessage);
-    getSkillInfos();
-  }, [location.search]);
+  // console.log(debug)
+  // console.log(skillArrays.length)
 
   return (
     <div className="app">
       <section className="output-container">
-        <ul className="tree">
-          {content?.map((skillInfo, idx) => (
-            <li key={idx} className={skillInfo.ParentID === "0" ? "parent" : ""}>
-              <div className="node">
-                <p>{`Skill: ${skillInfo.Skill}`}</p>
-                <p>{`SkillID: ${skillInfo.SkillID}`}</p>
-                <p>{`Parent: ${skillInfo.Parent}`}</p>
-                <p>{`ParentID: ${skillInfo.ParentID}`}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        {loading ? (
+          <div id='loading'>
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <ul className="tree">
+            {prevSkill?.map((skillInfo, idx) => 
+              <li key={idx} className={skillInfo.ParentID === '0' ? 'parent' : ''}>
+                <div className="node">
+                  <p>{`Skill: ${skillInfo.Skill}`}</p>
+                  <p>{`SkillID: ${skillInfo.SkillID}`}</p>
+                  <p>{`Parent: ${skillInfo.Parent}`}</p>
+                  <p>{`ParentID: ${skillInfo.ParentID}`}</p>
+                </div>
+              </li>
+            )}
+          </ul>
+        )}
       </section>
     </div>
   );
 };
+
+Output.propTypes = {
+  userInput: PropTypes.any
+}
 
 export default Output;
